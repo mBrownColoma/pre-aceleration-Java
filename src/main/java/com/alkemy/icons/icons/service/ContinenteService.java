79ad4eeb7f7f0de5b428caa6e8e7868d4e360e13
m.brown <m.brown@miguelbrown.cl>
2022-08-02 02:52:28 -0400

    creacion de servicios, y primeras pruebas con postman satisfactorias

---
+++ src/main/java/com/alkemy/icons/icons/service/ContinenteService.java
@@ -0,0 +1,15 @@
+package com.alkemy.icons.icons.service;
+
+import com.alkemy.icons.icons.dto.ContinenteDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContinenteService {
+
+    public ContinenteDTO save(ContinenteDTO dto){
+        //TODO: guardar continente
+        System.out.println("GUARDAR CONTINENTE");
+        return dto;
+    }
+
+}
